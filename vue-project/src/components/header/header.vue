49a0e50d5ae66cbe565bd616a8e077e5e1b32b77
Flutterain <1019226474@qq.com>
2018-07-12 16:03:35 +0800

    header and star finish

--- vue-project/src/components/header/header.vue
+++ vue-project/src/components/header/header.vue
@@ -16,32 +16,84 @@
           <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
-        <div class="support-count" v-if="seller.supports">
+        <div class="support-count" v-if="seller.supports" @click='showDetail'>
           <span class="count">{{seller.supports.length}}个</span>
           <i class="icon-keyboard_arrow_right"></i>
         </div>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click='showDetail'>
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width='100%' height='100%' alt="">
     </div>
+    <transition name="fade">
+      <div class="detail" v-show='detailShow'>
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h2 class="name">{{seller.name}}</h2>
+            <div class="star-wrapper">
+              <star :size='48' :score='seller.score'></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul class="supports" v-if='seller.supports'>
+              <li class="support-item" v-for='(item,index) in seller.supports' :key='index'>
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click='hideDetail'>
+          <i class="icon-close"></i>
+        </div>
+      </div>
+    </transition>  
   </div>
 </template>
 
 <script>
+  import star from '../star/star.vue'
+
   export default {
     props : {
       seller:{
         type:Object
       }
     },
+    data(){
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
+      },
+      hideDetail() {
+        this.detailShow = false
+      }
+    },
     created() {
       this.classMap = ['decrease','discount','special','invoice','guarantee']
     },
+    components: {
+      star
+    }
   }
 </script>
 
@@ -50,6 +102,7 @@
 
   .header
     position relative
+    overflow hidden
     color: #fff
     background: rgba(7,17,27,.5)
     .content-wrapper
@@ -160,7 +213,91 @@
       width 100%
       height 100%
       filter: blur(10px)
-
-
+    .detail
+      position fixed
+      z-index 100
+      top 0
+      left 0
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7, 17, 27, .8)
+      backdrop-filter blur(10px)
+      &.fade-enter-active, &.fade-leave-active
+        opacity 1
+        transition opacity .5s
+      &.fade-enter, &.fade-leave-to
+        opacity 0  
+      .detail-wrapper
+        width 100%
+        min-height 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            text-align center
+            font-size 16px
+            font-weight 700
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0 
+            text-align center 
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px
+            .line
+              flex 1
+              position relative
+              top -7px
+              border-bottom 1px solid rgba(255, 255, 255, .2)
+            .text
+              margin 0 12px
+              line-height 14px
+              font-size 14px
+              font-weight 700
+          .supports
+            width 80%
+            margin 0 auto 
+            .support-item
+              padding 0 12px 
+              margin-bottom 12px
+              font-size 0
+              &:last-child
+                margin-bottom 0
+              .icon
+                display inline-block
+                width 16px
+                height 16px
+                vertical-align top
+                background-size 16px 16px 
+                background-repeat no-repeat
+                &.decrease
+                  bg-image('./img/decrease_2')
+                &.discount
+                  bg-image('./img/discount_2')
+                &.special
+                  bg-image('./img/special_2')
+                &.invoice
+                  bg-image('./img/invoice_2')
+                &.guarantee
+                  bg-image('./img/guarantee_2')
+              .text
+                line-height 16px
+                margin-left 6px
+                font-size 12px 
+          .bulletin
+            width 80%
+            margin 0 auto 
+            .content
+              padding 0 12px
+              line-height 24px
+              font-size 12px
+      .detail-close
+        width 32px
+        height 32px
+        margin -64px auto 0
+        font-size 32px  
 </style>
 
