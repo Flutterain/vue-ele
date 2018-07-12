1024e025801c2d353a07a6d5a740f4f0a0c582cc
Flutterain <1019226474@qq.com>
2018-07-12 17:46:40 +0800

    goods start

--- vue-project/src/components/goods/goods.vue
+++ vue-project/src/components/goods/goods.vue
@@ -1,12 +1,51 @@
 <template>
-  <div>
-    这是商品
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="(item , index) in menu" :key="index" :class="{current:currentIndex === index}">
+          <span class="text">
+            <span class="icon" v-show="item.type > 0" :class="classMap[item.type]"></span>
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="food-wrapper">
+      <ul>
+        <li>
+          <h3></h3>
+          <ul>
+            <li></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  const ERR_OK = 0;
+
   export default {
-    
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods : []
+      }
+    },
+    created() {
+      this.classMap = ['decrease','discount','special','invoice','guarantee'];
+
+      this.$axios.get('/api/goods').then((response) => {
+        response = response.data;
+        if(response.errno === ERR_OK) {
+          this.goods = response.data;
+        }
+      })
+    }
   }
 </script>
 
