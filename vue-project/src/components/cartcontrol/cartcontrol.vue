e4abf12f8ab0460f28403276db9b5258e9e338f3
Flutterain <1019226474@qq.com>
2018-07-17 00:06:52 +0800

    完善cartcontrol

--- vue-project/src/components/cartcontrol/cartcontrol.vue
+++ vue-project/src/components/cartcontrol/cartcontrol.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="cartcontrol">
     <transition name="move">
-      <div class="cart-decrease" v-show="food.count > 0" @click.stop.prevent='decreaseCart'>
-        <transition name="rotate">
-          <span class="inner icon-remove_circle_outline"></span>
-        </transition>
+      <div class="cart-decrease" v-show="food.count > 0" @click.stop.prevent='decreaseCart'>        
+        <span class="inner icon-remove_circle_outline"></span>        
       </div>
     </transition>
     <div class="cart-count" v-show='food.count > 0'>{{food.count}}</div>
@@ -49,21 +47,23 @@
     .cart-decrease
       display inline-block
       padding 6px
-      &.move-enter-active, &.move-leave-active
-        transition all .3s linear 
-      &.move-enter, &.move-leave-to
-        opacity 1
-        transform translate3d(24px, 0, 0)
+      opacity 1
+      transform translate3d(0,0,0)
       .inner
         display inline-block
         line-height 24px
         font-size 24px
         color rgb(0, 160, 220)
-        &.rotate-enter-active, &.rotate-leave-active
-          transition all .3s linear
-          transform rotate(0)
-        &.rotate-enter, &.rotate-leave-to
+        transition all .4s linear
+        transform rotate(0)
+      &.move-enter-active, &.move-leave-active
+        transition all .4s linear 
+      &.move-enter, &.move-leave-to
+        opacity 0
+        transform translate3d(24px,0,0)
+        .inner
           transform rotate(180deg)
+
     .cart-count
       display inline-block
       width 12px
